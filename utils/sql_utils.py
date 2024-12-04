ccb3910c18071855957bd8c1453fac782c0fbec1
Sophia Guilbeault <sophiaguilbeault@crc-dot1x-nat-10-239-91-148.bu.edu>
2024-12-03 20:39:54 -0500

    update

--- utils/sql_utils.py
+++ utils/sql_utils.py
@@ -11,7 +11,7 @@ configure_logger(logger)
 
 
 # load the db path from the environment with a default value
-DB_PATH = os.getenv("DB_PATH", "/app/sql/meal_max.db")
+DB_PATH = os.getenv("DB_PATH", "/app/sql/affirmations.db")
 
 
 def check_database_connection():
